d8ef6ffb9d1006126b02537a9052d43d1f10ca32
tanukichi5000 <53566291+tanukichi5000@users.noreply.github.com>
2022-03-12 02:16:17 +0900

    Create send.php

---
+++ send.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="ja" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>サンプルページ</title>
+  </head>
+  <body>
+ 
+    <h1>送信完了</h1>
+ 
+    <p>お問い合わせありがとうございます。</p>
+    <p>送信が完了しました。</p>
+ 
+  </body>
+</html>
