cb22e7cddfc3ada044b6330ecd532f117ebbf34d
tanukichi5000 <53566291+tanukichi5000@users.noreply.github.com>
2022-03-12 02:14:49 +0900

    Create confirm.php

---
+++ confirm.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="ja" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>いつメンズメンバー募集</title>
+  </head><body><form action="send.php" method="post">
+名前：<input type="text" name="yourname" value="<?php echo $_POST['yourname']; ?>" disabled>><br>
+<label for="avatar">本人写真：</label>
+<input type="file"
+       id="avatar" name="avatar" value="<?php echo $_POST['avatar']; ?>" disabled>
+       accept="image/png, image/jpeg"><br>
+  
+性別を選択：<input id="male" type="radio" name="sex" value="<?php echo $_POST['sex']; ?>" disabled> value="male"><label for="male">男性</label>
+<input id="female" type="radio" name="sex" value="<?php echo $_POST['sex']; ?>" disabled> value="female"><label for="female">女性</label><br>
+<label for="num">年齢：</label>
+<input id="num" type="number" name="yoyaku-num">value="<?php echo $_POST['yoyaku-num']; ?>" disabled><br>
+<label for="mail">メールアドレス：</label>
+<input id="mail" type="email" name="mail">value="<?php echo $_POST['mail']; ?>" disabled><br>
+携帯電話番号（ハイフン不要）：<input type="text" name="yourname">value="<?php echo $_POST['yourname']; ?>" disabled><br>
+肩書き：<select name="sushi"value="<?php echo $_POST['sushi']; ?>" disabled>>
+  <option value="magro">クールな</option>
+  <option value="ika">キュートな</option>
+  <option value="ebi">紳士な</option>
+  <option value="ebi">VIPな</option>
+  <option value="ebi">ケチな</option>
+  <option value="ebi">魔法使い</option>
+  <option value="ebi">魔法少女</option>  
+  <option value="ebi">破壊神</option> 
+  <option value="ebi">エッチな</option>
+</select>
+<div>
+    生年月日：
+      <input type="date" name="dob"value="<?php echo $_POST['dob']; ?>" disabled>>
+     </div>
+郵便番号（ハイフン必須）：<input type="text" name="post" value="<?php echo $_POST['post']; ?>" disabled> pattern="\d{3}-\d{4}" title="郵便番号は、3桁の数字、ハイフン（-）、4桁の数字の順で記入してください。"><br>
+住所：<input type="text" name="yourname" value="<?php echo $_POST['yourname']; ?>" disabled>><br>  
+<label for="comment">コメント</label>
+<textarea id="comment" name="comment" value="<?php echo $_POST['comment']; ?>" disabled> placeholder="ここには自由にコメントを記入してください"></textarea><br><br>
+<input type="submit" value="送信"><br>   
+＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿<br>
+  <font color="red">ここより下の質問は他人（当サイトの管理者含む）には送信されません</font><br>
+秘密の質問：<select name="word">※漢字が分からない場合は、平仮名でも可  
+<option value="magro">自分の生まれ育った街の名前</option>
+<option value="magro">初恋の人の名前</option>
+<option value="magro">出身小学校</option>
+<option value="magro">出身中学校</option>
+<option value="magro">出身高校</option>
+<option value="magro">好きな芸能人の名前</option>
+<option value="magro">今まで自分が告白した回数</option>
+<option value="magro">今まで自分が告白された回数</option>
+<option value="magro">今まで自分が付き合った回数</option> 
+<option value="magro">好きなアニメキャラクター</option> 
+<option value="magro">中学生時代の好きな人</option>
+<option value="magro">人生で一番恥ずかしかったこと</option> 
+<option value="magro">自分の秘密</option>
+<br><br>
+ 秘密の質問の答え：<input type="text" name="yourname" value="<?php echo $_POST['yourname']; ?>" disabled>><br>
+  </form>
+</body></html>
+<br>
+<br>
+<center><font size="8">規約事項</font></center>
+<br>①当フォームで送信された個人情報等は、{いつメンズ住所録作成、いつメンズメンバー証}の作成に使用します。また、いつメンズ住所録に関してはその他メンバーにも公開させていただく場合がございます。
+<br>②退会手続きは現時点では、認められておりません。
+<br>③全項目に回答してください。記入漏れがあった場合は、上記のメールアドレスにお知らせいたします。メールに記載のURLより再度手続きを行ってください。
+<br>④アップロードされた本人写真は、メンバー証作成の為のみに使用されます。
+<br>⑤秘密の質問の答えは、今後本人確認が求められた場合にのみ使用されます。こちらの答えは、サイトの管理者には送信されませんので安心して入力ください。  
+<br>⑥秘密の質問の答えは、どこかにメモするなどして正確に覚えておいて下さい。一文字でも間違うと本人確認ができなくなります。
